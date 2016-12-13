2a401120d0007672e16a5f23907aef14823dfe8c
mithunder <mselvaratnam@gmail.com>
2016-12-13 12:46:55 -0500

    react and heroku

---
+++ browser/app.js
@@ -0,0 +1,14 @@
+// Libraries
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, IndexRedirect, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+
+
+render(
+  <Provider store={ store }>
+  <Router history={ browserHistory }>
+  </Router>
+</Provider>,
+document.getElementById('main')
+)
