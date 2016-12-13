ed76ec1fd536375521b4495d499e58c1325482dd
David Godow <dmgodow@gmail.com>
2016-12-13 13:20:40 -0500

    Initial routes

---
+++ server/models.js
@@ -0,0 +1,14 @@
+var Sequelize = require('sequelize');
+
+var db = new Sequelize('postgres://localhost:5432/recycling-app', {
+  logging: false
+});
+
+var RecyclingBins = db.define('recycling-bins', {
+  address: Sequelize.STRING,
+  borough: Sequelize.STRING,
+  latitude: Sequelize.FLOAT,
+  longitude: Sequelize.FLOAT,
+  park_site_name: Sequelize.STRING,
+  site_type: Sequelize.STRING
+});
